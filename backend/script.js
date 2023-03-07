f09765d190f86f8630e31e0345ad173bfa1e0860
Richardrafael <richard.rafael.soares@gmail.com>
2023-03-07 08:18:29 -0300

    function tag

--- backend/script.js
+++ backend/script.js
@@ -1,11 +1,14 @@
 function começa() {
     console.log("passei")
 }
-
+começa()
 
 function nova() {
 console.log("nova branch");
 }
+nova()
 
-
-
+function tag() {
+    console.log("tag")
+}
+tag()
