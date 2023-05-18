4178e348cbcfbe03a6ff68b8ae263b26955399bc
Richardrafael <richard.rafael.soares@gmail.com>
2023-05-18 17:40:58 -0300

    test

---
+++ list/README.md
@@ -0,0 +1,6 @@
+# Persistencia-de-dados
+Repositório para persistência de dados da aplicação (livros, categorias, etc.)
+
+## teste
+testando token 
+
---
+++ list/src/models/services.ts
@@ -0,0 +1,21 @@
+import { MariaDBDataSource } from "./data_source";
+import {Livro} from "./model";
+
+export class Service{    
+    start(){       
+            MariaDBDataSource.initialize().then( ()=>{
+                console.log("Inicializada a fonte de dados...");
+            }).catch((err)=>{
+                console.error("Erro de inicialização da fonte de dados!!");
+            }) 
+    }
+    async insert(livro: Livro){
+        await MariaDBDataSource.manager.save(livro);        
+    }
+    async listAll(){
+       let list = await MariaDBDataSource.manager.find(Livro);
+       return list;
+    }
+    
+}
+
